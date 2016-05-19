a7a1cfbd0fb9eaca95039bcb0d479ffbdca417a4
Rafael Nowrotek <mail@benignware.com>
2016-05-19 20:10:36 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    watch: {
+      build: {
+        files: ['src/**/*'],
+        tasks: ['build']
+      }
+    },
+    clean: {
+      build: 'dist' 
+    },
+    sync: {
+      build: {
+        files: [{
+          expand: true, 
+          cwd: 'src',
+          src: ["**/*", "!vendor", "!assets/**/*.js", "!assets/**/*.less", "!templates/**"], 
+          dot: true,
+          dest: "dist"
+        }],
+        pretend: false, // Don't do any IO. Before you run the task with `updateAndDelete` PLEASE MAKE SURE it doesn't remove too much. 
+        verbose: true // Display log messages when copying files 
+      }
+    },
+    mincerrc: {
+      options: {
+        manifest: false,
+        digest: false,
+        include: [
+          'vendor/assets/components',
+          'src/assets/javascripts',
+          'src/assets/stylesheets'
+        ],
+        clean: false,
+        sourceMaps: false,
+      },
+      build: {
+        cwd: 'src',
+        src: ['**/application.*'],
+        dest: 'dist'
+      }
+    },
+    assemble: {
+      options: {
+        flatten: true,
+        assets: 'assets',
+        helpers: ['src/templates/helpers/*.js', 'handlebars-helpers'],
+        partials: ['src/templates/partials/**/*.hbs'],
+        layout: ['src/templates/layouts/default.hbs'],
+        data: ['src/templates/data/*.{json,yml}']
+      },
+      build: {
+        src: ['src/templates/pages/**/*.hbs'],
+        dest: 'dist'
+      }
+    },
+    connect: {
+      build: {
+        options: {
+          open: true,
+          livereload: true,
+          base: 'dist/',
+          port: 9090
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  
+  grunt.loadNpmTasks('grunt-sync');
+  grunt.loadNpmTasks('grunt-mincerrc');
+  grunt.loadNpmTasks('grunt-assemble');
+
+  grunt.registerTask('default', ['jshint']);
+  
+  grunt.registerTask('build', ['jshint', 'clean:build', 'sync:build', 'mincerrc:build', 'assemble:build']);
+  
+  grunt.registerTask('serve', ['connect:build', 'watch:build']);
+
+};
---
+++ src/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+/**
+ *= require jquery/dist/jquery.min.js
+ *= require bootstrap/dist/js/bootstrap.min.js
+ *= require_self
+ */
+$(function() {
+  // Ready.
+});
